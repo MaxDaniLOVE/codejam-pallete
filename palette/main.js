53d482c60b55876156abc386c542c1e33ed007e9
unknown <mahseeen@gmail.com>
2019-11-05 21:58:07 +0300

    feat: add changeColor method

--- palette/main.js
+++ palette/main.js
@@ -4,18 +4,32 @@ class Canvas {
         this.chooseColorActive = false;
         this.moveActive = false;
         this.transformActive = false;
-
+        // tools buttons
         this.bucketBtn = document.querySelector('.bucket');
         this.chooseColorBtn = document.querySelector('.choose-color');
         this.moveBtn = document.querySelector('.move');
         this.transformBtn = document.querySelector('.transform');
+        // color buttons
+        this.currentColorBtn = document.querySelector('.btn-current-color');
+        this.prevColorBtn = document.querySelector('.btn-prev-color');
+        this.firstColorBtn = document.querySelector('.btn-first-color');
+        this.secondColorBtn = document.querySelector('.btn-second-color');
+        // color spans
+        this.activeSpan = document.querySelector('.current-color')
+        this.prevSpan = document.querySelector('.prev-color')
+        this.firstSpan = document.querySelector('.first-color')
+        this.secondSpan = document.querySelector('.second-color')
+        // active and previous colors 
+        this.activeColor = window.getComputedStyle(this.activeSpan).backgroundColor
+        this.prevColor = window.getComputedStyle(this.prevSpan).backgroundColor
     }
     getBlocks(){
+        canvas.setTool()
         let blocks = document.querySelectorAll('.block');
         blocks.forEach(block => {
             block.addEventListener('click', () => {
                 if (this.bucketActive) {
-                    block.style.background = 'red'
+                    block.style.background = `${this.activeColor}`
                 }
                 if (this.transformActive) {
                     block.classList.toggle('block-rounded')
@@ -50,9 +64,21 @@ class Canvas {
             this.transformActive = true;
         })
     }
+    changeColor(){
+        let colorButtons = document.querySelectorAll('.btn-color')
+        colorButtons.forEach(colorBtn => {
+            colorBtn.addEventListener('click', () => {
+                this.prevColor = this.activeColor
+                this.prevSpan.style.backgroundColor = this.prevColor
+
+                this.activeColor = window.getComputedStyle(colorBtn.firstChild).backgroundColor;
+                this.activeSpan.style.backgroundColor = this.activeColor
+            })
+        });
+        this.activeSpan.style.backgroundColor = `${this.activeColor}` 
+    }
 }
 
 let canvas = new Canvas()
 canvas.getBlocks()
-canvas.setTool()
-console.log(canvas.transformActive);
+canvas.changeColor()
